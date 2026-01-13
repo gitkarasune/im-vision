544e347b6b07188a1f62650005bb11188dcf7f89
Kara Sune <sunebarikara@gmail.com>
2026-01-13 10:29:36 +0100

    fix the reset-psd build-time error

--- app/reset-password/page.tsx
+++ app/reset-password/page.tsx
@@ -1,9 +1,12 @@
+import { Suspense } from "react"
 import { ResetPasswordForm } from "@/components/auth/reset-password-form"
 
 export default function LoginPage() {
   return (
     <div className="bg-white dark:bg-black">
-      <ResetPasswordForm />
+      <Suspense fallback={<div>Loading...</div>}>
+        <ResetPasswordForm />
+      </Suspense>
     </div>
   )
 }
